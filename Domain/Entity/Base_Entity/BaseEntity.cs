172fb177d331c36a6da3c4d05c75c456cfaec80e
Nitin <nitanshuchoudhary003@gmail.com>
2024-01-05 13:49:25 +0530

    Initial Commit

---
+++ Domain/Entity/Base_Entity/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity.Base_Entity
+{
+    public class BaseEntity<PrimaryKey>
+    {
+        public PrimaryKey Id { get; set; }
+    }
+}
---
+++ Domain/Entity/Register.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entity.Base_Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Register:BaseEntity<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string PassworHash { get; set; }
+    }
+}
